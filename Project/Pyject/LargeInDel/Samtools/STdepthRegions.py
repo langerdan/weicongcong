472db0f1e8b9e84a312e654ac305cc3db7ab7450
codeunsolved <codeunsolved@gmail.com>
2016-08-01 16:44:38 +0800

    01/08/16 mod com_with

---
+++ Project/Pyject/LargeInDel/Samtools/STdepthRegions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PROGRAM  : STdepthRegions_v0.01a
+# PURPOSE  :
+# AUTHOR   : codeunsolved@gmail.com
+# CREATED  : August 1 2016
+
+import os
+import re
+import sys
+import subprocess
+
--- Project/com_with.sh
+++ Project/com_with.sh
@@ -4,13 +4,22 @@ com_line=$1
 with_obj=$2
 if [ $# -eq 3 ];
     then
-    o_flag=$3
-    if [[ $o_flag =~ '^([\+\-])(.+)$' ]]; then
-        direction=$match[1]
+    operations=$3
+    redi_flag=''
+    if [[ $operations =~ '^>(.+)$' ]]; then
+        operations=$match[1]
+        redi_flag='>'
+    fi
+    if [[ $operations =~ '^([+])(.+)$' ]]; then
+        mod_flag=$match[1]
         tag=.$match[2]
-    elif [[ $o_flag =~ '^([\+\-])$' ]]; then
-        direction=$match[1]
+    elif [[ $operations =~ '^([-=])$' ]]; then
+        mod_flag=$match[1]
         tag=''
+    elif [[ $operations =~ '^([#])(.+)#(.+)$' ]]; then
+        mod_flag=$match[1]
+        tag_ori=$match[2]
+        tag_mod=$match[3]
     fi
 fi
 
@@ -21,17 +30,26 @@ do
         echo "=>Command: $com_line $file"
         eval $com_line $file
     elif [ $# -eq 3 ]; then
-        if [[ $direction =~ '\+' ]]; then
-            echo "=>Command: $com_line $file >${file_basename}${tag}"
-            eval $com_line $file '>'${file_basename}${tag}
-        elif [[ $direction =~ '\-' ]]; then
-            if [[ $file_basename =~ "^(.+)$tag" ]]; then
+        if [[ $mod_flag =~ '\+' ]]; then
+            echo "=>Command: $com_line $file ${redi_flag}${file_basename}${tag}"
+            eval $com_line $file ${redi_flag}${file_basename}${tag}
+        elif [[ $mod_flag =~ '\-' ]]; then
+            if [[ $file_basename =~ "^(.+)\.[^.]+$" ]]; then
                 file_o_name=$match[1]
-                echo "=>Command: $com_line $file >${file_o_name}"
-                eval $com_line $file '>'${file_o_name}
+                echo "=>Command: $com_line $file ${redi_flag}${file_o_name}"
+                eval $com_line $file ${redi_flag}${file_o_name}
+            fi
+        elif [[ $mod_flag =~ '\=' ]]; then
+            echo "=>Command: $com_line $file ${redi_flag}${file_basename}"
+            eval $com_line $file ${redi_flag}${file_basename}
+        elif [[ $mod_flag =~ '\#' ]]; then
+            if [[ $file_basename =~ "^(.+)$tag_ori" ]]; then
+                file_o_name=$match[1]
+                echo "=>Command: $com_line $file ${redi_flag}${file_o_name}${tag_mod}"
+                eval $com_line $file ${redi_flag}${file_o_name}${tag_mod}
             fi
         else
-            echo "**ERROR** incorrect direction flag! got $direction, accept + or -!"
+            echo "**ERROR** incorrect mod flag! got \"$operations\", accept +, - or #!"
         fi
     else
         echo "**ERROR** incorrect arguments number! got $#, need 2 or 3!"
