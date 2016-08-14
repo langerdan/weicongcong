1a248fdd8c6c69fd3503c37c05c6a583f962cbad
codeunsolved <codeunsolved@gmail.com>
2016-08-15 07:08:07 +0800

    15/08/16

--- Project/Pyject/LargeInDel/countReads_bed+fg.py
+++ Project/Pyject/LargeInDel/countReads_bed+fg.py
@@ -34,6 +34,38 @@ def read_bed(path_b):
     return a_details
 
 
+def read_fg_list(path_fg_l):
+    fg_d = {"fg_a": {}, "fg_b": {}}
+    with open(path_fg_l, 'rb') as r_obj_fg:
+        for line_fg in r_obj_fg:
+            fg_a = re.match('([^\t]+)\t', line_fg).group(1)
+            fg_b = re.match('[^\t]+\t([^\t]+)', line_fg).group(1)
+            chr_n = re.match('(?:[^\t]+\t){2}([^\t]+)', line_fg).group(1)
+            pos_s = int(re.match('(?:[^\t]+\t){3}([^\t]+)', line_fg).group(1))
+            pos_e = int(re.match('(?:[^\t]+\t){4}([^\t\n\r]+)', line_fg).group(1))
+            if fg_a not in fg_d["fg_a"]:
+                fg_d["fg_a"][fg_a] = {}
+                fg_d["fg_a"][fg_a]["ln-b"] = []
+            fg_d["fg_a"][fg_a]["ln-b"].append("%s-%s-%s" % (fg_b, chr_n, pos_s))
+            fg_d["fg_b"]["%s-%s-%s" % (fg_b, chr_n, pos_s)] = {"chr": chr_n, "pos_s": pos_s, "pos_e": pos_e}
+    return fg_d
+
+
+def get_fg_a_cpp(a_details, fg_d):
+    for a_key in a_details:
+        chr_n = a_details[a_key][0]
+        gene_name = a_details[a_key][3]
+        pos_s = a_details[a_key][1]
+        pos_e = a_details[a_key][2]
+        if gene_name in ["ALK", "FGFR3", "ROS1", "RET", "NTRK1"]:
+            if gene_name not in fg_d["fg_a"]:
+                fg_d["fg_a"][gene_name] = {}
+            fg_d["fg_a"][gene_name]["chr"] = chr_n
+            fg_d["fg_a"][gene_name]["pos_s"] = pos_s
+            fg_d["fg_a"][gene_name]["pos_e"] = pos_e
+    return fg_d
+
+
 def parse_cigar(operations, len_valid):
     # print operations
     if re.match('\d+\w', operations):
@@ -63,9 +95,9 @@ def save_tab(y_data, x_axis, tab_name):
                 sum_row += each_sample[key]
                 sum_col_list[y_data.index(each_sample)] += each_sample[key]
             sum_row_list.append(sum_row)
-            #print "amplicon: %s sum is %d" % (key, sum_row)
+            print "amplicon: %s sum is %d" % (key, sum_row)
             w_obj.write('\n')
-        #print "%d samples sum are: %s" % (len(x_axis), sum_col_list)
+        print "%d samples sum are: %s" % (len(x_axis), sum_col_list)
 
     # write tab - nli-sample-sum
     with open(os.path.join(dir_sam, "%s-nli-sample-sum" % tab_name), 'wb') as w_obj:
@@ -166,15 +198,27 @@ def output_mismatch(data, log_name):
         w_obj.write("%s\t%d\n" % ("@Mismatch", mismatch_num))
 
 
+def output_fg(data, output_name):
+    data_sorted = sorted(data, key=lambda d: (d[0], d[1]))
+    with open(os.path.join(dir_sam, output_name), 'wb') as w_obj:
+        for each_row in data_sorted:
+            w_obj.write("%s\n" % [str(x) for x in each_row].split("\t"))
+
+
 if __name__ == '__main__':
     path_bed = sys.argv[1]
     dir_sam = sys.argv[2]
-    print "=>path of *.bed: %s\n=>dir of *.sam: %s" % (path_bed, dir_sam)
+    path_fg_list = sys.argv[3]
+    print "=>path of *.bed: %s\n=>dir of *.sam: %s=>path of fusion gene list: %s" % (path_bed, dir_sam, path_fg_list)
 
     print "=>get amplicon details...",
     amplicon_details = read_bed(path_bed)
     print "OK!"
-    #print amplicon_details
+    # print amplicon_details
+    print "=>get fusion gene list...",
+    fusion_gene_d = read_fg_list(path_fg_list)
+    fusion_gene_d = get_fg_a_cpp(amplicon_details, fusion_gene_d)
+    print "OK!"
 
     print "=>get all paths of *.sam file..."
     path_sam_list = get_file_path(dir_sam, "sam", 'list', 2)
@@ -183,6 +227,7 @@ if __name__ == '__main__':
 
     amplicon_stat = []
     sam_basename_list = []
+
     for each_p_sam in path_sam_list:
         print "[No.%d/%d]processing with %s..." % (path_sam_list.index(each_p_sam) + 1, len(path_sam_list), each_p_sam)
         sam_basename_list.append(re.match('([^_]+_[^_]+)', os.path.basename(each_p_sam)).group(1))
@@ -194,6 +239,10 @@ if __name__ == '__main__':
         with open(each_p_sam, 'rb') as r_obj_sam:
             counts = {}
             mismatch = {}
+            fg_b_hit = {}
+            fg_a_hit = {}
+            fg_seq = {}
+            fg_output = []
             line_i = 0
             for line_sam in r_obj_sam:
                 line_i += 1
@@ -204,6 +253,7 @@ if __name__ == '__main__':
                 cigar = re.match('(?:[^\t]+\t){5}([^\t]+)', line_sam).group(1)
                 len_match = parse_cigar(cigar, 0)
                 pos_end = pos_start + len_match
+                seq_name = re.match('([^\t]+)\t', line_sam).group(1)
                 log_each = "%s-(%s-%s-%s)-%s" % (pos_start, chr_name, cigar, len_match, pos_end)
                 match_trigger = 0
                 for each_key in amplicon_details:
@@ -215,13 +265,40 @@ if __name__ == '__main__':
                             counts[each_key] += 1
                             match_trigger = 1
                             break
+                for gene_b_name in fusion_gene_d["fg_b"]:
+                    if gene_b_name not in fg_b_hit:
+                        fg_b_hit[gene_b_name] = []
+                    if chr_name == fusion_gene_d["fg_b"][gene_b_name]["chr"]:
+                        if pos_start >= fusion_gene_d["fg_b"][gene_b_name]["pos_s"] and \
+                                                pos_end - 1 <= fusion_gene_d["fg_b"][gene_b_name]["pos_e"]:
+                            fg_b_hit[gene_b_name].append(seq_name)
+                            fg_seq[seq_name] = line_sam
+                for fusion_gene_a in fusion_gene_d["fg_a"]:
+                    if fusion_gene_a not in fg_a_hit:
+                        fg_a_hit[fusion_gene_a] = []
+                    if chr_name == fusion_gene_d["fg_a"][fusion_gene_a]["chr"]:
+                        if pos_start >= fusion_gene_d["fg_a"][fusion_gene_a]["pos_s"] and \
+                                                pos_end - 1 <= fusion_gene_d["fg_a"][fusion_gene_a]["pos_e"]:
+                            fg_a_hit[fusion_gene_a].append(seq_name)
                 if match_trigger == 0:
                     if log_each not in mismatch:
                         mismatch[log_each] = 1
                     else:
                         mismatch[log_each] += 1
             mismatch["@TotalReads"] = line_i
-            output_mismatch(mismatch, "%s-mismatch.log" % os.path.basename(each_p_sam))
+            for fusion_gene_a in fusion_gene_d["fg_a"]:
+                for fusion_gene_b in fusion_gene_d["fg_a"][fusion_gene_a]["ln-b"]:
+                    for seq_a in fg_a_hit[fusion_gene_a]:
+                        for seq_b in fg_b_hit[fusion_gene_b]:
+                            if seq_a == seq_b:
+                                print "fg match: %s" % seq_b
+                                gene_b_name = re.match('([^\-]+)', fusion_gene_b).group(1)
+                                fg_output.append([fusion_gene_a, gene_b_name,
+                                                  fusion_gene_d["fg_b"][fusion_gene_b]["chr"],
+                                                  fusion_gene_d["fg_b"][fusion_gene_b]["pos_s"],
+                                                  fusion_gene_d["fg_b"][fusion_gene_b]["pos_e"], fg_seq[seq_b]])
+        output_mismatch(mismatch, "%s-mismatch.log" % os.path.basename(each_p_sam))
+        output_fg(fg_output, "%s-fusion-gene.log" % os.path.basename(each_p_sam))
         amplicon_stat.append(counts)
 
     amplicon_details_sorted = sorted(amplicon_details.iteritems(), key=lambda d: (d[1][0], d[1][1]))
