72976de09bb4cb9493ff8aea4a3c4aa3a3143f87
codeunsolved <codeunsolved@gmail.com>
2016-10-08 22:16:52 +0800

    08/10/16

--- Project/LargeInDel/AIH/transPrimerSheet.py
+++ Project/LargeInDel/AIH/transPrimerSheet.py
@@ -12,30 +12,30 @@ import sys
 input_file = sys.argv[1]
 print 'processing with %s...' % input_file
 if re.match('[/~]', input_file):
-	print 'perhaps you use absolute path,'
-	path_input = input_file
+    print 'perhaps you use absolute path,'
+    path_input = input_file
 else:
-	print 'perhaps you use relative path,'
-	path_input = os.path.join(os.getcwd(), input_file)
+    print 'perhaps you use relative path,'
+    path_input = os.path.join(os.getcwd(), input_file)
 
 
 amplicon_info_file = []
-with open(path_input, 'rb') as r_obj:
-	for line_no, line in enumerate(r_obj):
-		if line_no == 0:
-			amplicon_info_file.append('AmpliconNmae\tPrimer1Sequence\tPrimer2Sequence\tAmpliconLength\tChr\t'
-									  'GenomicAmpiconStart\tGenomicAmpiconEnd\n')
-			continue
-		amplicon_name = re.match('(?:[^\t]+\t){6}([^\t]+)', line).group(1)
-		primer1seq = re.match('(?:[^\t]+\t){1}([^\t]+)', line).group(1)
-		primer2seq = re.match('(?:[^\t]+\t){2}([^\t]+)', line).group(1)
-		amplicon_len = re.match('(?:[^\t]+\t){10}([^\t]+)', line).group(1)
-		chr_no = re.match('(?:[^\t]+\t){5}([^\t]+)', line).group(1)
-		amplicon_start = re.match('(?:[^\t]+\t){6}([^\t]+)', line).group(1)
-		amplicon_end = re.match('(?:[^\t]+\t){9}([^\t]+)', line).group(1)
-		amplicon_info_file.append('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (amplicon_name, primer1seq, primer2seq, amplicon_len,
-																    chr_no, amplicon_start, amplicon_end))
+with open(path_input, 'rb') as amp_info:
+    for line_no, line in enumerate(amp_info):
+        if line_no == 0:
+            amplicon_info_file.append('AmpliconNmae\tPrimer1Sequence\tPrimer2Sequence\tAmpliconLength\tChr\t'
+                                      'GenomicAmpiconStart\tGenomicAmpiconEnd\n')
+            continue
+        amplicon_name = re.match('(?:[^\t]+\t){6}([^\t]+)', line).group(1)
+        primer1seq = re.match('(?:[^\t]+\t){1}([^\t]+)', line).group(1)
+        primer2seq = re.match('(?:[^\t]+\t){2}([^\t]+)', line).group(1)
+        amplicon_len = re.match('(?:[^\t]+\t){10}([^\t]+)', line).group(1)
+        chr_no = re.match('(?:[^\t]+\t){5}([^\t]+)', line).group(1)
+        amplicon_start = re.match('(?:[^\t]+\t){6}([^\t]+)', line).group(1)
+        amplicon_end = re.match('(?:[^\t]+\t){9}([^\t]+)', line).group(1)
+        amplicon_info_file.append('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (amplicon_name, primer1seq, primer2seq, amplicon_len,
+                                                                    chr_no, amplicon_start, amplicon_end))
 
-with open(os.path.join(os.getcwd(), 'amplicon_info'), 'wb') as w_obj:
-	for line in amplicon_info_file:
-		w_obj.write(line)
+with open(os.path.join(os.getcwd(), 'amplicon_info'), 'wb') as bed:
+    for line in amplicon_info_file:
+        bed.write(line)
--- Project/countReads.py
+++ Project/countReads.py
@@ -27,185 +27,185 @@ len_primer = 35
 
 
 def save_tab(y_data, x_axis, tab_name):
-	sum_row_list = []
-	sum_col_list = [0 for x in x_axis]
-	# write tab - reads
-	with open(os.path.join(dir_sam, "%s-reads" % tab_name), 'wb') as w_obj:
-		w_obj.write("\t%s\n" % '\t'.join(x_axis))
-		for key, value in amplicon_details_sorted:
-			w_obj.write(key)
-			sum_row = 0
-			for each_sample in y_data:
-				w_obj.write("\t%d" % each_sample[key])
-				sum_row += each_sample[key]
-				sum_col_list[y_data.index(each_sample)] += each_sample[key]
-			sum_row_list.append(sum_row)
-			# print "amplicon: %s sum is %d" % (key, sum_row)
-			w_obj.write('\n')
-		# print "%d samples sum are: %s" % (len(x_axis), sum_col_list)
+    sum_row_list = []
+    sum_col_list = [0 for x in x_axis]
+    # write tab - reads
+    with open(os.path.join(dir_sam, "%s-reads" % tab_name), 'wb') as reads:
+        reads.write("\t%s\n" % '\t'.join(x_axis))
+        for key, value in amplicon_details_sorted:
+            reads.write(key)
+            sum_row = 0
+            for sample in y_data:
+                reads.write("\t%d" % sample[key])
+                sum_row += sample[key]
+                sum_col_list[y_data.index(sample)] += sample[key]
+            sum_row_list.append(sum_row)
+            # print "amplicon: %s sum is %d" % (key, sum_row)
+            reads.write('\n')
+        # print "%d samples sum are: %s" % (len(x_axis), sum_col_list)
 
-	# write tab - nli-sample-sum
-	with open(os.path.join(dir_sam, "%s-nli-sample-sum" % tab_name), 'wb') as w_obj:
-		w_obj.write("\t%s\n" % '\t'.join(x_axis))
-		for key, value in amplicon_details_sorted:
-			w_obj.write(key)
-			for each_sample in y_data:
-				sum_sample = sum_col_list[y_data.index(each_sample)]
-				if sum_sample != 0:
-					w_obj.write("\t%d" % (each_sample[key] / sum_sample * 10000))
-				else:
-					w_obj.write("\t0")
-			w_obj.write('\n')
+    # write tab - nli-sample-sum
+    with open(os.path.join(dir_sam, "%s-nli-sample-sum" % tab_name), 'wb') as nli_ss:
+        nli_ss.write("\t%s\n" % '\t'.join(x_axis))
+        for key, value in amplicon_details_sorted:
+            nli_ss.write(key)
+            for sample in y_data:
+                sum_sample = sum_col_list[y_data.index(sample)]
+                if sum_sample != 0:
+                    nli_ss.write("\t%d" % (sample[key] / sum_sample * 10000))
+                else:
+                    nli_ss.write("\t0")
+            nli_ss.write('\n')
 
-	# write tab - nli-amplicon-aver
-	with open(os.path.join(dir_sam, "%s-nli-amplicon-aver" % tab_name), 'wb') as w_obj:
-		w_obj.write("\t%s\n" % '\t'.join(x_axis))
-		for key, value in amplicon_details_sorted:
-			w_obj.write(key)
-			for each_sample in y_data:
-				aver_reads = sum_row_list[amplicon_details_sorted.index((key, value))] / len(x_axis)
-				if aver_reads != 0:
-					w_obj.write("\t%d" % (each_sample[key] / aver_reads))
-				else:
-					w_obj.write("\t0")
-			w_obj.write('\n')
+    # write tab - nli-amplicon-aver
+    with open(os.path.join(dir_sam, "%s-nli-amplicon-aver" % tab_name), 'wb') as nli_aa:
+        nli_aa.write("\t%s\n" % '\t'.join(x_axis))
+        for key, value in amplicon_details_sorted:
+            nli_aa.write(key)
+            for sample in y_data:
+                aver_reads = sum_row_list[amplicon_details_sorted.index((key, value))] / len(x_axis)
+                if aver_reads != 0:
+                    nli_aa.write("\t%d" % (sample[key] / aver_reads))
+                else:
+                    nli_aa.write("\t0")
+            nli_aa.write('\n')
 
 
 def save_stat(y_data, x_axis):
-	def set_style(height=210, bold=False, color_index=xlwt.Style.colour_map['black'], name='Microsoft YaHei UI'):
-		style = xlwt.XFStyle()
-		font = xlwt.Font()
-		font.name = name
-		font.bold = bold
-		font.colour_index = color_index
-		font.height = height
-		style.font = font
-		return style
+    def set_style(height=210, bold=False, color_index=xlwt.Style.colour_map['black'], name='Microsoft YaHei UI'):
+        style = xlwt.XFStyle()
+        font = xlwt.Font()
+        font.name = name
+        font.bold = bold
+        font.colour_index = color_index
+        font.height = height
+        style.font = font
+        return style
 
-	path_xls = os.path.join(dir_sam, 'reads_stat.xls')
-	workbook = xlwt.Workbook(style_compression=2)
+    path_xls = os.path.join(dir_sam, 'reads_stat.xls')
+    workbook = xlwt.Workbook(style_compression=2)
 
-	sheet1 = workbook.add_sheet(u'reads', cell_overwrite_ok=True)
-	sheet2 = workbook.add_sheet(u'nli-sample-sum', cell_overwrite_ok=True)
-	sheet3 = workbook.add_sheet(u'nli-amplicon-aver', cell_overwrite_ok=True)
+    sheet1 = workbook.add_sheet(u'reads', cell_overwrite_ok=True)
+    sheet2 = workbook.add_sheet(u'nli-sample-sum', cell_overwrite_ok=True)
+    sheet3 = workbook.add_sheet(u'nli-amplicon-aver', cell_overwrite_ok=True)
 
-	sum_row_list = [0 for x in amplicon_details_sorted]
-	sum_col_list = []
-	# write sheet1 - reads
-	for key, value in amplicon_details_sorted:
-		sheet1.write(amplicon_details_sorted.index((key, value)) + 1, 0, key, set_style(220, True))
-	for x_no, x in enumerate(x_axis):
-		sheet1.write(0, x_no + 1, x, set_style(220, True))
-		sum_col = 0
-		for key, value in amplicon_details_sorted:
-			sheet1.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, y_data[x_no][key])
-			sum_col += y_data[x_no][key]
-			sum_row_list[amplicon_details_sorted.index((key, value))] += y_data[x_no][key]
-		sum_col_list.append(sum_col)
-		print "sample: %s sum is %d" % (x, sum_col)
-	print "%d amplicons sum are: %s" % (len(amplicon_details_sorted), sum_row_list)
+    sum_row_list = [0 for x in amplicon_details_sorted]
+    sum_col_list = []
+    # write sheet1 - reads
+    for key, value in amplicon_details_sorted:
+        sheet1.write(amplicon_details_sorted.index((key, value)) + 1, 0, key, set_style(220, True))
+    for x_no, x in enumerate(x_axis):
+        sheet1.write(0, x_no + 1, x, set_style(220, True))
+        sum_col = 0
+        for key, value in amplicon_details_sorted:
+            sheet1.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, y_data[x_no][key])
+            sum_col += y_data[x_no][key]
+            sum_row_list[amplicon_details_sorted.index((key, value))] += y_data[x_no][key]
+        sum_col_list.append(sum_col)
+        print "sample: %s sum is %d" % (x, sum_col)
+    print "%d amplicons sum are: %s" % (len(amplicon_details_sorted), sum_row_list)
 
-	# write sheet2 - nli-sample-sum
-	for key, value in amplicon_details_sorted:
-		sheet2.write(amplicon_details_sorted.index((key, value)) + 1, 0, key, set_style(220, True))
-	for x_no, x in enumerate(x_axis):
-		sheet2.write(0, x_no + 1, x, set_style(220, True))
-		sample_sum = sum_col_list[x_no]
-		for key, value in amplicon_details_sorted:
-			if sample_sum != 0:
-				sheet2.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, y_data[x_no][key] / sample_sum * 10000)
-			else:
-				sheet2.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, 0)
+    # write sheet2 - nli-sample-sum
+    for key, value in amplicon_details_sorted:
+        sheet2.write(amplicon_details_sorted.index((key, value)) + 1, 0, key, set_style(220, True))
+    for x_no, x in enumerate(x_axis):
+        sheet2.write(0, x_no + 1, x, set_style(220, True))
+        sample_sum = sum_col_list[x_no]
+        for key, value in amplicon_details_sorted:
+            if sample_sum != 0:
+                sheet2.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, y_data[x_no][key] / sample_sum * 10000)
+            else:
+                sheet2.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, 0)
 
-	# write_sheet3 - nli-amplicon-aver
-	for x_no, x in enumerate(x_axis):
-		sheet3.write(0, x_no + 1, x, set_style(220, True))
-	for key, value in amplicon_details_sorted:
-		for x_no, x in enumerate(x_axis):
-			sheet3.write(amplicon_details_sorted.index((key, value)) + 1, 0, key, set_style(220, True))
-			amplicon_aver = sum_row_list[amplicon_details_sorted.index((key, value))] / len(x_axis)
-			if amplicon_aver != 0:
-				sheet3.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, y_data[x_no][key] / amplicon_aver)
-			else:
-				sheet3.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, 0)
+    # write_sheet3 - nli-amplicon-aver
+    for x_no, x in enumerate(x_axis):
+        sheet3.write(0, x_no + 1, x, set_style(220, True))
+    for key, value in amplicon_details_sorted:
+        for x_no, x in enumerate(x_axis):
+            sheet3.write(amplicon_details_sorted.index((key, value)) + 1, 0, key, set_style(220, True))
+            amplicon_aver = sum_row_list[amplicon_details_sorted.index((key, value))] / len(x_axis)
+            if amplicon_aver != 0:
+                sheet3.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, y_data[x_no][key] / amplicon_aver)
+            else:
+                sheet3.write(amplicon_details_sorted.index((key, value)) + 1, x_no + 1, 0)
 
-	workbook.save(path_xls)
+    workbook.save(path_xls)
 
 
 def output_mismatch(data, log_name):
-	data_sorted = sorted(data.iteritems(), key=lambda d: d[0])
-	mismatch_num = 0
-	with open(os.path.join(dir_sam, log_name), 'wb') as w_obj:
-		for d_key, d_value in data_sorted:
-			w_obj.write("%s\t%d\n" % (d_key, d_value))
-			if not re.match('@', d_key):
-				mismatch_num += d_value
-		w_obj.write("%s\t%d\n" % ("@Mismatch", mismatch_num))
+    data_sorted = sorted(data.iteritems(), key=lambda d: d[0])
+    mismatch_num = 0
+    with open(os.path.join(dir_sam, log_name), 'wb') as mismatch:
+        for d_key, d_value in data_sorted:
+            mismatch.write("%s\t%d\n" % (d_key, d_value))
+            if not re.match('@', d_key):
+                mismatch_num += d_value
+        mismatch.write("%s\t%d\n" % ("@Mismatch", mismatch_num))
 
 
 if __name__ == '__main__':
-	print "========================================================"
-	print datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
-	print "========================================================"
-	print "=>path of *.bed: %s\n=>dir of *.sam: %s" % (path_bed, dir_sam)
+    print "========================================================"
+    print datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
+    print "========================================================"
+    print "=>path of *.bed: %s\n=>dir of *.sam: %s" % (path_bed, dir_sam)
 
-	print "=>get amplicon details...",
-	amplicon_details = read_bed(path_bed)
-	print "OK!"
-	# print amplicon_details
+    print "=>get amplicon details...",
+    amplicon_details = read_bed(path_bed)
+    print "OK!"
+    # print amplicon_details
 
-	print "=>get all paths of *.sam file..."
-	path_sam_list = get_file_path(dir_sam, "sam", 'list', 1)
-	dir_basename = os.path.basename(dir_sam)
-	print "OK! total %d." % len(path_sam_list)
-	print "========================================================"
+    print "=>get all paths of *.sam file..."
+    path_sam_list = get_file_path(dir_sam, "sam", 'list', 1)
+    dir_basename = os.path.basename(dir_sam)
+    print "OK! total %d." % len(path_sam_list)
+    print "========================================================"
 
-	amplicon_stat = []
-	sam_basename_list = []
-	for each_p_sam in path_sam_list:
-		print "[No.%d/%d]processing with %s..." % (path_sam_list.index(each_p_sam) + 1, len(path_sam_list), each_p_sam)
-		sam_basename = re.match('(.+)\.sam', os.path.basename(each_p_sam)).group(1)
-		if re.search('_S\d+', sam_basename):
-			sam_basename = re.match('(.+)_S\d+', sam_basename).group(1)
-		sam_basename_list.append(sam_basename)
-		n = 35 if re.match('BRCA', dir_basename) else 0
-		with open(each_p_sam, 'rb') as r_obj_sam:
-			counts = {}
-			mismatch = {}
-			line_i = 0
-			for line_sam in r_obj_sam:
-				line_i += 1
-				if re.match('@', line_sam):
-					continue
-				chr_name = re.match('(?:[^\t]+\t){2}([^\t]+)', line_sam).group(1)
-				pos_start = int(re.match('(?:[^\t]+\t){3}([^\t]+)', line_sam).group(1))
-				cigar = re.match('(?:[^\t]+\t){5}([^\t]+)', line_sam).group(1)
-				len_match = parse_cigar_len(cigar, 0)
-				pos_end = pos_start + len_match - 1
-				match_trigger = 0
-				for each_key in amplicon_details:
-					if each_key not in counts:
-						counts[each_key] = 0
-					if chr_name == amplicon_details[each_key][0]:
-						if pos_start >= amplicon_details[each_key][2] - n and \
-						pos_end <= amplicon_details[each_key][3] + n:
-							counts[each_key] += 1
-							match_trigger = 1
-							break
-				# log misnatch
-				log_each = "%s-(%s-%s-%s)-%s" % (pos_start, chr_name, cigar, len_match, pos_end)
-				if match_trigger == 0:
-					if log_each not in mismatch:
-						mismatch[log_each] = 1
-					else:
-						mismatch[log_each] += 1
-			mismatch["@TotalReads"] = line_i
-			output_mismatch(mismatch, "%s-mismatch.log" % re.match('(.+)\.sam', os.path.basename(each_p_sam)).group(1))
+    amplicon_stat = []
+    sam_basename_list = []
+    for p_sam in path_sam_list:
+        print "[No.%d/%d]processing with %s..." % (path_sam_list.index(p_sam) + 1, len(path_sam_list), p_sam)
+        sam_basename = re.match('(.+)\.sam', os.path.basename(p_sam)).group(1)
+        if re.search('_S\d+', sam_basename):
+            sam_basename = re.match('(.+)_S\d+', sam_basename).group(1)
+        sam_basename_list.append(sam_basename)
+        n = 35 if re.match('BRCA', dir_basename) else 0
+        with open(p_sam, 'rb') as sam:
+            counts = {}
+            mismatch = {}
+            line_i = 0
+            for line_sam in sam:
+                line_i += 1
+                if re.match('@', line_sam):
+                    continue
+                chr_name = re.match('(?:[^\t]+\t){2}([^\t]+)', line_sam).group(1)
+                pos_start = int(re.match('(?:[^\t]+\t){3}([^\t]+)', line_sam).group(1))
+                cigar = re.match('(?:[^\t]+\t){5}([^\t]+)', line_sam).group(1)
+                len_match = parse_cigar_len(cigar, 0)
+                pos_end = pos_start + len_match - 1
+                match_trigger = 0
+                for key in amplicon_details:
+                    if key not in counts:
+                        counts[key] = 0
+                    if chr_name == amplicon_details[key][0]:
+                        if pos_start >= amplicon_details[key][2] - n and \
+                        pos_end <= amplicon_details[key][3] + n:
+                            counts[key] += 1
+                            match_trigger = 1
+                            break
+                # log mismatch
+                log_mis = "%s-(%s-%s-%s)-%s" % (pos_start, chr_name, cigar, len_match, pos_end)
+                if match_trigger == 0:
+                    if log_mis not in mismatch:
+                        mismatch[log_mis] = 1
+                    else:
+                        mismatch[log_mis] += 1
+            mismatch["@TotalReads"] = line_i
+            output_mismatch(mismatch, "%s-mismatch.log" % re.match('(.+)\.sam', os.path.basename(p_sam)).group(1))
 
-		amplicon_stat.append(counts)
+        amplicon_stat.append(counts)
 
-	amplicon_details_sorted = sorted(amplicon_details.iteritems(), key=lambda d: (d[1][0], d[1][1]))
-	print "output table..."
-	#save_tab(amplicon_stat, sam_basename_list, 'reads_statistics')
-	save_stat(amplicon_stat, sam_basename_list)
-	print "========================================================\nOK!"
+    amplicon_details_sorted = sorted(amplicon_details.iteritems(), key=lambda d: (d[1][0], d[1][1]))
+    print "output table..."
+    #save_tab(amplicon_stat, sam_basename_list, 'reads_statistics')
+    save_stat(amplicon_stat, sam_basename_list)
+    print "========================================================\nOK!"
